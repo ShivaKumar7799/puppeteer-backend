2a1b7c00054e4fdd66d2ba161bc900ad7edba2bd
ShivaKumar7799 <shivakumarbammidi@gmail.com>
2024-11-08 23:58:21 +0530

    czc

--- index.js
+++ index.js
@@ -9,27 +9,37 @@ const puppeteer = require('puppeteer-core');
 const app = express();
 const port = 8000;
 
+// Enable CORS
 app.use(
   cors({ origin: '*', methods: 'GET,POST', allowedHeaders: 'Content-Type' })
 );
+
+// Parse JSON bodies
 app.use(bodyParser.json());
 
 app.post('/generate-pdf', async (req, res) => {
   try {
-    const htmlContent = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>Document</title></head><body><h1>Sample PDF Content</h1></body></html>`;
+    const htmlContent = `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head><meta charset="UTF-8"><title>Document</title></head>
+        <body><h1>كيف حالك</h1></body>
+      </html>`;
 
     const browser = await puppeteer.launch({
-      executablePath: await chromium.executablePath,
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
       headless: chromium.headless,
     });
 
     const page = await browser.newPage();
     await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
 
-    const pdfPath = path.join(__dirname, 'output.pdf');
-    await page.pdf({ path: pdfPath, format: 'A4', printBackground: true });
+    const pdfBuffer = await page.pdf({
+      format: 'A4',
+      printBackground: true,
+    });
 
     await browser.close();
 
@@ -37,18 +47,14 @@ app.post('/generate-pdf', async (req, res) => {
       'Content-Type': 'application/pdf',
       'Content-Disposition': 'attachment; filename=output.pdf',
     });
-    const pdfStream = fs.createReadStream(pdfPath);
-    pdfStream.pipe(res);
 
-    pdfStream.on('end', () => {
-      fs.unlinkSync(pdfPath);
-    });
+    res.send(pdfBuffer);
   } catch (error) {
     console.error('Error generating PDF:', error);
     res.status(500).send('Error generating PDF');
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+app.listen(port, () =>
+  console.log(`Server is running on http://localhost:${port}`)
+);
